08fecc9e5101a3edbc564fd6cd9884c4b26a417d
Mobinul Kawshik <31078662+Mobinul341@users.noreply.github.com>
2017-10-29 15:49:32 +0600

    Update problem_solving.py

--- problem_solving.py
+++ problem_solving.py
@@ -1,2 +1,28 @@
 # Problem-Solution
-This is about problem solving on Python
+#This is about problem solving on Python
+
+#1. 1st problem
+print("""Hello World
+This is my first program""")
+
+#2. 2nd problem
+print()
+print("Hello World \nThis is my first program")
+
+#3. 3rd problem
+print("The question is - 'How to write a #comment in Python programming language?'")
+print("Answer is- '#'This sign indicates single line comment\n'''...''' This sign indicates multiline comment")
+
+#4. 4th Problem
+a = 5
+b = 3.1416
+c = 'a'
+print('The value is',type(a),':', a)
+print('The value is', type(b),':', b)
+print('The value is', type(c), ':', c)
+
+#5. 5th problem
+age = int(input("Enter your age: "))
+print("Your age is: ", age)
+
+#6. 6th 
