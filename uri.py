ff1ead159f836d885874f8f07e01e9d8cc92cf1b
Mobinul Kawshik <31078662+Mobinul341@users.noreply.github.com>
2017-11-08 09:38:35 +0600

    This is about Uri online judge
    This is about Uri online judge

---
+++ uri.py
@@ -0,0 +1,82 @@
+'''
+#1001
+A = int(input())
+B = int(input())
+X = eval(str(A+B))
+print("X =",X)
+'''
+'''
+#1002
+R = float(input())
+A = 3.14159*R**2
+print('A' + '=' + '%.4f'%A)
+'''
+'''
+#1003
+A = int(input())
+B = int(input())
+SOMA = A+B
+print("SOMA =", SOMA)
+'''
+'''
+#1004
+A = int(input())
+B = int(input())
+PROD = A*B
+print("PROD =", PROD)
+'''
+'''
+#1005
+A = float(input())
+B = float(input())
+MEDIA = ((round(A, 1)/11*3.5) + (round(B, 1)/11*7.5))
+print('MEDIA = '+'%.5f'%MEDIA)
+'''
+'''
+#1006
+A = float(input())
+B = float(input())
+C = float(input())
+MEDIA = ((round(A, 1)/10*2) + (round(B, 1)/10*3) + (round(C, 1)/10*5))
+print('MEDIA = '+'%.1f'%MEDIA)
+'''
+'''
+#1007
+A = int(input())
+B = int(input())
+C = int(input())
+D = int(input())
+DIFERENCA = ((A*B)-(C*D))
+print("DIFERENCA =", DIFERENCA)
+'''
+'''
+#1008
+a = int(input())
+b = int(input())
+c = float(input())
+salary = b*c
+print("NUMBER =",a)
+print("SALARY = U$",'%.2f'%salary)
+'''
+'''
+#1009
+a = input()
+b = float(input())
+c = float(input())
+total = round(b,2)+round(c,2)*0.15
+print("TOTAL = R$", '%.2f'%total)
+'''
+#1010
+a = int(input())
+b = int(input())
+c = float(input())
+result = b* round(c, 2)
+print("VALOR A PAGAR: R$",'%.2f'%result)
+
+
+
+
+
+
+
+
